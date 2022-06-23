d77bd5e2620da2d7d881b3241dd747ca26af0c37
Yunier Alvarez <yunieralvarez@gmail.com>
2022-06-22 22:11:34 -0400

    clear on space press

--- src/components/TypingBoard.tsx
+++ src/components/TypingBoard.tsx
@@ -1,11 +1,22 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createSignal, JSX } from 'solid-js';
 
 export const TypingBoard: Component = () => {
   const [text, setText] = createSignal('');
 
+  const handleOnKeyDown: JSX.EventHandlerUnion<
+    HTMLInputElement,
+    KeyboardEvent
+  > = event => {
+    event.key === ' ' && setText('');
+  };
+
   return (
     <>
-      <input onInput={e => setText(e.currentTarget.value)} />
+      <input
+        value={text()}
+        onInput={e => setText(e.currentTarget.value)}
+        onKeyDown={handleOnKeyDown}
+      />
       <p>{text()}</p>
     </>
   );
