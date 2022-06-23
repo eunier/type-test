9875cf92800212d48680015561ea44fadf6e591b
Yunier Alvarez <yunieralvarez@gmail.com>
2022-06-22 22:27:16 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import type { Component } from 'solid-js';
+import { faker } from '@faker-js/faker';
+import { Component, createSignal } from 'solid-js';
 
 import styles from './App.module.css';
 import { TextDisplay } from './components/TextDisplay';
 import { TypingBoard } from './components/TypingBoard';
 
 const App: Component = () => {
+  const [words] = createSignal(
+    Array(50)
+      .fill(null)
+      .map(() => faker.random.word())
+  );
+
+  const text = () => words().join(' ').toLowerCase();
+
   return (
     <div class={styles.App}>
-      <TextDisplay />
+      <TextDisplay text={text()} />
       <TypingBoard />
     </div>
   );
--- src/components/TextDisplay.tsx
+++ src/components/TextDisplay.tsx
@@ -1,14 +1,5 @@
-import { faker } from '@faker-js/faker';
-import { Component, createSignal } from 'solid-js';
+import { Component } from 'solid-js';
 
-export const TextDisplay: Component = () => {
-  const [words] = createSignal(
-    Array(50)
-      .fill(null)
-      .map(() => faker.random.word())
-  );
-
-  const text = () => words().join(' ').toLowerCase();
-
-  return <p>{text()}</p>;
+export const TextDisplay: Component<{ text: string }> = props => {
+  return <p>{props.text}</p>;
 };
