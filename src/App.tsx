a3da79a896b6a59ac61511c3f9892e5308fe44b5
Yunier Alvarez <yunieralvarez@gmail.com>
2022-06-22 21:16:23 -0400

    add typeing board

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import type { Component } from 'solid-js';
 
 import styles from './App.module.css';
 import { TextDisplay } from './components/TextDisplay';
+import { TypingBoard } from './components/TypingBoard';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <TextDisplay />
+      <TypingBoard />
     </div>
   );
 };
---
+++ src/components/TypingBoard.tsx
@@ -0,0 +1,5 @@
+import { Component } from 'solid-js';
+
+export const TypingBoard: Component = () => {
+  return <div>type here</div>;
+};
