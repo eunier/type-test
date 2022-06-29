f4a76d7d92e4d7ce13636b803218eda74fe17b7e
Yunier Alvarez <yunieralvarez@gmail.com>
2022-06-28 22:48:53 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { faker } from '@faker-js/faker';
+import { pipe } from 'fp-ts/lib/function';
+import { map, range } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 import { Component, createSignal } from 'solid-js';
-
 import styles from './App.module.css';
 import { TextDisplay } from './components/TextDisplay';
 import { TypingBoard } from './components/TypingBoard';
+
+const test = pipe(
+  range(0, 49),
+  map(i => faker.random.word())
+);
+
+console.log({ test });
 
 const App: Component = () => {
   const [words] = createSignal(
@@ -11,6 +19,7 @@ const App: Component = () => {
       .fill(null)
       .map(() => faker.random.word().toLowerCase())
   );
+  const [test] = createSignal(range(0, 49));
 
   const [targetWordIndex, setTargetWordIndex] = createSignal(0);
   const targetWord = () => words()[targetWordIndex()];
