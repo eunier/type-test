161f9ebbc065a82c7cb59d92c8ebcbf63b90c173
Yunier Alvarez <yunieralvarez@gmail.com>
2022-07-02 16:56:03 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,7 @@ import { TypingBoard } from './components/TypingBoard';
 
 const App: Component = () => {
   const [words] = createSignal(
-    pipe(
-      RNEA.range(0, 49),
-      RNEA.map(() => faker.random.word()),
-      RNEA.map(toLowerCase)
-    )
+    pipe(RNEA.range(0, 49), RNEA.map(faker.random.word), RNEA.map(toLowerCase))
   );
 
   const [targetWordIndex, setTargetWordIndex] = createSignal(0);
