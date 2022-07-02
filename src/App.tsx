e9efe5fcdb43a4280dbd7fc1ce62439396c1f07e
Yunier Alvarez <yunieralvarez@gmail.com>
2022-07-02 17:39:20 -0400

    text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
-import { pipe } from 'fp-ts/lib/function';
+import { pipe } from 'fp-ts/function';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import { toLowerCase } from 'fp-ts/string';
+import * as S from 'fp-ts/string';
 import { Component, createSignal } from 'solid-js';
 import styles from './App.module.css';
 import { TextDisplay } from './components/TextDisplay';
@@ -9,12 +9,21 @@ import { TypingBoard } from './components/TypingBoard';
 
 const App: Component = () => {
   const [words] = createSignal(
-    pipe(RNEA.range(0, 49), RNEA.map(faker.random.word), RNEA.map(toLowerCase))
+    pipe(
+      RNEA.range(0, 49),
+      RNEA.map(faker.random.word),
+      RNEA.map(S.toLowerCase)
+    )
   );
 
   const [targetWordIndex, setTargetWordIndex] = createSignal(0);
   const targetWord = () => words()[targetWordIndex()];
-  const text = () => words().join(' ');
+
+  const text = () =>
+    pipe(
+      words(),
+      RNEA.reduce('', (b, a) => `${b} ${a}`)
+    );
 
   return (
     <div class={styles.App}>
